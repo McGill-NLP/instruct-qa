b99faf776a5e5a4ad04631dba782b56763267da4
xhluca <git@xinghanlu.com>
2023-08-09 17:34:05 -0400

    Add files necessary for pypi publishing

---
+++ instruct_qa/version.py
@@ -0,0 +1,5 @@
+__version__ = "0.0.1"
+
+# NOTE: Do not modify this manually. Version is automatically updated in
+# .github/scripts/update_version.py when .github/workflows/publish-python.yml 
+# is run.
